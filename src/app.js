0d241bc4b37ddff875fd515710e23f7489124ff7
avysel <avanryssel@gmail.com>
2019-04-15 11:27:01 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,66 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var stringify = require('json-stringify-safe');
+var payablehello = require('./payablehello'); // app services
 
+var app = express();
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+/**
+* Display home page
+*/
+app.get('/', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+/**
+* Get name
+*/
+app.get('/name', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+/**
+* Update name
+*/
+app.post('/name', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+
+/**
+* Update name with raw transaction
+*/
+app.post('/name/raw', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+/**
+* Withdraw contract balance
+*/
+app.get('/withdraw', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+/**
+* Get server status
+*/
+app.get('/status', function(req, res) {
+	var parameters = getParameters(null);
+	res.render('index', parameters);
+});
+
+// init blockchain connection
+payablehello.connection();
+payablehello.initContracts();
+payablehello.connectionInfo();
+
+// start server
+app.listen(3000);
