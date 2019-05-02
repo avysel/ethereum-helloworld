e0204a6763d9b9773bb7b49d1e346e835a645bb1
VANRYSSEL Alexandre <avanryssel@gmail.com>
2019-05-02 16:57:41 +0200

    oracle

---
+++ src/api.js
@@ -0,0 +1,25 @@
+/**
+* Simple API that returns a new string every minute
+*/
+
+var express = require('express');
+var stringify = require('json-stringify-safe');
+var config = require("./config.js");
+
+var app = express();
+
+app.get('/name', function(req, res) {
+	console.log("GET /name");
+	var responseBody = new Object();
+	var today = new Date();
+
+	// name is Toto-yyyymmddhhmm
+	responseBody.name = "Toto-"+today.getFullYear()+(today.getMonth()+1)+today.getDate()+today.getHours()+today.getMinutes()+today.getSeconds();
+	console.log("Response : "+responseBody.name);
+	res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(responseBody));
+
+});
+
+// start server on port 3001
+app.listen(3001);
---
+++ src/oracle.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var config = require("./config.js");
+var payableHello = require('./payablehello'); // app services
+
+
+
+async function readAPI() {
+
+	request({url: 'http://127.0.0.1:3001/name', json: true}, async function(err, res, json) {
+		 if (err) {
+			console.error(err);
+		}
+
+		var name = json.name;
+		var price = "2";
+
+		await payableHello.updateName(name, price)
+		.then(
+			(result) => {
+				console.log(result);
+			},
+			(error) => {
+				console.error(error);
+			}
+		);
+    });
+
+}
+
+// init blockchain connection
+payableHello.connection();
+payableHello.initContracts();
+
+setInterval(readAPI, 1000 * 10);
