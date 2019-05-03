02ad41d48da965abe0dc4b3338a2572c8d441b85
VANRYSSEL Alexandre <avanryssel@gmail.com>
2019-05-03 16:44:34 +0200

    tests

--- test/payablehello-test.js
+++ test/payablehello-test.js
@@ -6,48 +6,52 @@ function t(n) {
 }
 
 contract("PayableHello", async accounts => {
-  it("should display 'nobody' when no name set", async () => {
+
+	it("should display 'nobody' when no name set", async () => {
 		let hello = await Hello.deployed();
 		let name = await hello.getName.call();
 		assert.equal(name, "nobody","name wasn't nobody");
-   });
+	});
 
-  it("should pay to change the name and display the new name", async () => {
-    let newName = "toto";
-    let price = "2";
-	let hello = await Hello.deployed();
-	let gas = await hello.setName.estimateGas(newName, { from: accounts[0], gas: 4000000, value: web3.utils.toWei(price, "ether") }	);
-	let txHash = await hello.setName.sendTransaction(newName, { from: accounts[0], gas: gas, value: web3.utils.toWei(price, "ether") });
-	let name = await hello.getName.call();
-	assert.equal(name, newName,"name wasn't changed" );
-  });
-
-  it("should not change the name because don't pay enough", async () => {
-    let newName = "titi";
-    let price = "1";
-	let hello = await Hello.deployed();
-	let oldName = await hello.getName.call();
-	let name = null;
-
-	try {
+	it("should pay to change the name and display the new name", async () => {
+		let newName = "toto";
+		let price = "2";
+		let hello = await Hello.deployed();
 		let gas = await hello.setName.estimateGas(newName, { from: accounts[0], gas: 4000000, value: web3.utils.toWei(price, "ether") }	);
 		let txHash = await hello.setName.sendTransaction(newName, { from: accounts[0], gas: gas, value: web3.utils.toWei(price, "ether") });
-	}
-	catch(error) {}
+		let name = await hello.getName.call();
+		assert.equal(name, newName,"name wasn't changed" );
+	});
 
-	name = await hello.getName.call();
-	assert.equal(name, oldName,"name was changed" );
-	assert.notEqual(name, newName,"name was changed" );
-  });
+	it("should not change the name because don't pay enough", async () => {
+		let newName = "titi";
+		let price = "1";
+		let hello = await Hello.deployed();
+		let oldName = await hello.getName.call();
+		let name = null;
+
+		try {
+			let gas = await hello.setName.estimateGas(newName, { from: accounts[0], gas: 4000000, value: web3.utils.toWei(price, "ether") }	);
+			let txHash = await hello.setName.sendTransaction(newName, { from: accounts[0], gas: gas, value: web3.utils.toWei(price, "ether") });
+			assert.equal(1,2,"exception should have been thrown !");
+		}
+		catch(error) {
+			assert.equal(1,1,"we should get a catch !");
+		}
+
+		name = await hello.getName.call();
+		assert.equal(name, oldName,"name was changed" );
+		assert.notEqual(name, newName,"name was changed" );
+	});
 
 
-  it("should withdraw contract balance", async () => {
+	it("should withdraw contract balance", async () => {
 		let hello = await Hello.deployed();
 		let initialContractBalance = await web3.eth.getBalance(hello.address);
 		let initialUserBalance = await web3.eth.getBalance(accounts[0]);
 
 		let gas = await hello.withdraw.estimateGas({ from: accounts[0], gas: 4000000 }	);
-        let receipt = await hello.withdraw.sendTransaction( { from: accounts[0], gas: gas*2 });
+		let receipt = await hello.withdraw.sendTransaction( { from: accounts[0], gas: gas*2 });
 
 		let gasUsed = receipt.receipt.gasUsed;
 		let tx = await web3.eth.getTransaction(receipt.tx);
@@ -59,9 +63,9 @@ contract("PayableHello", async accounts => {
 
 		assert.equal(finalContractBalance, 0,"contract balance is not 0");
 		assert.equal(t(finalUserBalance), t(parseInt(initialContractBalance,10) + parseInt(initialUserBalance,10) - parseInt(txPrice,10)) ,"user balance is not initial balance + contract balance - tx price");
-  });
+	});
 
-  it("should not withdraw contract balance", async () => {
+	it("should not withdraw contract balance", async () => {
 		let hello = await Hello.deployed();
 		let initialContractBalance = await web3.eth.getBalance(hello.address);
 		let initialUserBalance = await web3.eth.getBalance(accounts[1]);
@@ -83,6 +87,6 @@ contract("PayableHello", async accounts => {
 		let finalUserBalance = await web3.eth.getBalance(accounts[1]);
 
 		assert.equal(finalContractBalance, initialContractBalance,"contract balance is not unchanged");
-  });
+	});
 
 });
